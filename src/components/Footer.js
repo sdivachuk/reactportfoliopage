52eb9c48ce6729c1e3fdb056d8e5cc1757dd1a38
Sergey Divachuk <sergeydivachuk@gmail.com>
2022-07-31 23:07:05 -0400

    added links and logos for github and linked in

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 const Footer = () => {
   return (
-    <div id='footerDiv'>
-      <h1>Footer</h1>
-
+    <div id="footerDiv">
+      <a href="https://github.com/sdivachuk" target="_blank">
+        <img id="gitlogo" src="/githubLogo2.png" alt="github"></img>
+      </a>
+      <a href="https://www.linkedin.com/in/sergey-d-418703173/" target="_blank">
+        <img id="linkedinlogo" src="/linkedin.png" alt="linked In"></img>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
